58c9183e880042e087b2185edb19d4c68b8fd682
Kokored7214 <kokored69166434@gmail.com>
2020-08-01 13:37:52 +0800

    Simplified console player position

--- src/main/java/kokored/koko_keywordblocker/events/KeywordBlocker.java
+++ src/main/java/kokored/koko_keywordblocker/events/KeywordBlocker.java
@@ -15,17 +15,23 @@ public class KeywordBlocker implements Listener {
         Player player = (Player)  event.getPlayer();
         String message = event.getMessage();
 
-        for (int i = 0 ; i < plugin.getConfig().getStringList("keyword").size() ; i++){
-            if (message.contains(plugin.getConfig().getStringList("keyword").get(i))){
-                event.setCancelled(true);
-                player.sendMessage(ChatColor.RED + "該用詞遭到屏蔽 無法發出!");
-                player.sendMessage(ChatColor.RED + "The word is blocked, cannot be sent!");
-                System.out.println("[Koko_KeywordBlocker] -------------------------------------------------------------------");
-                System.out.println("[Koko_KeywordBlocker] Player " + player.getDisplayName() + " sent a blocked message!");
-                System.out.println("[Koko_KeywordBlocker] Message content: " + message);
-                System.out.println("[Koko_KeywordBlocker] Player at: " + player.getLocation());
-                System.out.println("[Koko_KeywordBlocker] Player's login IP: " + player.getAddress());
-                System.out.println("[Koko_KeywordBlocker] -------------------------------------------------------------------");
+        int x = player.getLocation().getBlockX();
+        int y = player.getLocation().getBlockY();
+        int z = player.getLocation().getBlockZ();
+
+        if (plugin.getConfig().getBoolean("enable")) {
+            for (int i = 0 ; i < plugin.getConfig().getStringList("keyword").size() ; i++){
+                if (message.contains(plugin.getConfig().getStringList("keyword").get(i))){
+                    event.setCancelled(true);
+                    player.sendMessage(ChatColor.RED + "該用詞遭到屏蔽 無法發出!");
+                    player.sendMessage(ChatColor.RED + "The word is blocked, cannot be sent!");
+                    System.out.println("[Koko_KeywordBlocker] -------------------------------------------------------------------");
+                    System.out.println("[Koko_KeywordBlocker] Player " + player.getDisplayName() + " sent a blocked message!");
+                    System.out.println("[Koko_KeywordBlocker] Message content: " + message);
+                    System.out.println("[Koko_KeywordBlocker] Player at: " + " world:" + player.getLocation().getWorld().getName() + " X:" + x + " Y:" + y + " Z:" + z);
+                    System.out.println("[Koko_KeywordBlocker] Player's login IP: " + player.getAddress());
+                    System.out.println("[Koko_KeywordBlocker] -------------------------------------------------------------------");
+                }
             }
         }
     }
